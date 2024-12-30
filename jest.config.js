56218cdf840d32eb2ce226fa7dc24c627ceaeb10
Sylent-Sys <renaldiapriyanto419@gmail.com>
2024-12-30 12:41:08 +0700

    Refactor configuration files to support ES modules and update package version to 1.0.2

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
-module.exports = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   transform: {
-    "^.+\\.ts?$": "ts-jest",
+    "^.+\\.ts?$": "ts-jest"
   },
   transformIgnorePatterns: ["<rootDir>/node_modules/"]
 }
--- rollup.config.js
+++ rollup.config.js
@@ -4,58 +4,54 @@ import { terser } from "rollup-plugin-terser"
 import json from "rollup-plugin-json"
 import copy from "rollup-plugin-copy2"
 import typescript from "@rollup/plugin-typescript"
-import tar from "tar"
+import * as tar from "tar"
 import fs from "fs"
-import pkg from "./package.json"
+import pkg from "./package.json" assert { type: "json" }
+
 import crypto from "crypto"
 
-// Custom plugin to clean the dist folder before building
 const clean = () => ({
   buildStart() {
     const dist = "./dist/"
     if (fs.existsSync(dist)) {
-      fs.readdirSync(dist).forEach(path => {
-        if (path.endsWith(".tar.gz")) {
-          fs.unlinkSync(dist + path)
+      fs.readdirSync(dist).forEach(file => {
+        if (file.endsWith(".tar.gz")) {
+          fs.unlinkSync(dist + file)
         }
       })
     }
   },
 })
 
-// Custom plugin to hash the JS bundle and write it in the schema
 const hash = () => ({
   writeBundle() {
-    // Generate JS hash
     const fileBuffer = fs.readFileSync("dist/plugin.min.js")
     const hashSum = crypto.createHash("sha1")
     hashSum.update(fileBuffer)
     const hex = hashSum.digest("hex")
 
-    // Read and parse existing schema from dist folder
-    const schema = JSON.parse(fs.readFileSync("./dist/schema.json", "utf8"))
+    const schemaPath = "./dist/schema.json"
+    const schema = JSON.parse(fs.readFileSync(schemaPath, "utf8"))
 
-    // Write updated schema to dist folder, pretty printed as JSON again
     const newSchema = {
       ...schema,
       hash: hex,
       version: pkg.version,
     }
-    fs.writeFileSync("./dist/schema.json", JSON.stringify(newSchema, null, 2))
+    fs.writeFileSync(schemaPath, JSON.stringify(newSchema, null, 2))
   },
 })
 
-// Custom plugin to bundle up our files after building
 const bundle = () => ({
   async writeBundle() {
     const bundleName = `${pkg.name}-${pkg.version}.tar.gz`
     return tar
-        .c({ gzip: true, cwd: "dist" }, [
-          "plugin.min.js",
-          "schema.json",
-          "package.json",
-        ])
-        .pipe(fs.createWriteStream(`dist/${bundleName}`))
+      .c({ gzip: true, cwd: "dist" }, [
+        "plugin.min.js",
+        "schema.json",
+        "package.json",
+      ])
+      .pipe(fs.createWriteStream(`dist/${bundleName}`))
   },
 })
 
@@ -98,6 +94,6 @@ export default {
       assets: ["schema.json", "package.json"],
     }),
     hash(),
-    bundle()
+    bundle(),
   ],
 }
